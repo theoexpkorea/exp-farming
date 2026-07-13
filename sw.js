060bfba46d028ae74e7f0d5a49c3ea01987532cb
theoexpkorea <koreakjh7@gmail.com>
2026-07-14 02:43:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
-const CACHE = 'farming-search-v2';
+const CACHE = 'farming-search-v3';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
