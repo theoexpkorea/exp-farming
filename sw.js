52789fe07f7183bcecdf67574cdbbb7c1d4597da
theoexpkorea <koreakjh7@gmail.com>
2026-07-13 22:08:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 파밍서치 서비스워커 — 앱 셸 캐시 (데이터는 항상 네트워크 우선)
 const CACHE = 'farming-search-v1';
-const SHELL = ['./index.html', './manifest.json'];
+const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
