e5ed3a1434f537500835bfa45fd4fcd7a3a95090
theoexpkorea <koreakjh7@gmail.com>
2026-07-14 03:11:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'farming-search-v3';
+const CACHE = 'farming-search-v4';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
